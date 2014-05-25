22588c6e61838aa7ab404555ad539bde15b6ea61
Nelson-Mark <nelson.mark1990@gmail.com>
2014-05-26 01:13:25 +0530

    Update cachematrix.R
    Added the comments for functioning of the modules and its answer key

--- cachematrix.R
+++ cachematrix.R
@@ -1,8 +1,13 @@
-## Put comments here that give an overall description of what your
-## functions do
+## These comments have been inserted by Mark Nelson @ nelson.mark1990@gmail.com
 
-## Write a short comment describing this function
+## There are two functions in this R File, makeCacheMatrix and cacheSolve
 
+## function 1 : makeCacheMatrix
+## This function defines an empty matrix "x" which is set to work for 2 X 2 matrix
+## The Set Sub Function takes 4 numbers as input and converts it to a 2 X 2 square matrix
+## The get function allows the users to be able to see the converted matrix
+## The setinv function sets the inverse of this matrix using the solve function which is built-in into R
+## The getinv functions outputs the inverted matrix
 makeCacheMatrix <- function(x = matrix()) {
     m <- NULL
     set <- function(y) {
@@ -21,8 +26,10 @@ makeCacheMatrix <- function(x = matrix()) {
 
 
 
-## Write a short comment describing this function
-
+## Function 2: cacheSolve
+## This function first checks to see if the inverted mstrix is already stored as cache in variable "m"
+## If the value is present then it displays the value and comment instead of recomputing the inverse again
+## If the value is missing, then it actually calucluates the inverse of the matrix and displays it
 cacheSolve <- function(x, ...) {
   ## Return a matrix that is the inverse of 'x'
   m <- x$getinv()
@@ -36,3 +43,32 @@ cacheSolve <- function(x, ...) {
   m
   
 }
+
+## You can run the following commands to Cross Check the Answer:
+## a <- makeCacheMatrix()
+## a$set(c(1,2,3,4))
+## a$get()
+##                        Output:
+##                         a$get()
+##                       [,1] [,2]
+##                  [1,]    1    3
+##                  [2,]    2    4
+
+## Running the Solution
+
+##                  Output - First time 
+##                  cacheSolve(a)
+##                       [,1] [,2]
+##                  [1,]   -2  1.5
+##                  [2,]    1 -0.5
+
+
+##                  Output - Second time 
+##                  cacheSolve(a)
+##                  getting cached data
+##                       [,1] [,2]
+##                  [1,]   -2  1.5
+##                  [2,]    1 -0.5
+
+
+
